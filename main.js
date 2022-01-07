2929402257c4bcf044dd162373935a28c81191c1
Emerson Pessoa <60630092+emersonpessoa01@users.noreply.github.com>
2022-01-07 18:24:17 -0300

    Optimized

--- main.js
+++ main.js
@@ -1,72 +1,99 @@
 "use strict";
 
 const openModal = () =>
-  document.getElementById("modal").classList.add("active");
+    document.getElementById("modal").classList.add("active");
 
+//-=-=-limpa os campos e depois fecha o modal-=-=-
 const closeModal = () => {
-  clearFields();
-  document.getElementById("modal").classList.remove("active");
+    clearInputs(); //limpa os campos
+    document.getElementById("modal").classList.remove("active");
 };
 
-const getLocalStorage = () =>
-  JSON.parse(localStorage.getItem("db_aluno")) ?? [];
-const setLocalStorage = (dbAluno) =>
-  localStorage.setItem("db_aluno", JSON.stringify(dbAluno));
-
-// CRUD - create read update delete
-const deleteAluno = (index) => {
-  const dbAluno = readAluno();
-  dbAluno.splice(index, 1);
-  setLocalStorage(dbAluno);
+//-=-=-=-VARIAVEL GLOBAL de aluno temporario-=-=-=-
+/*
+const tempAluno = {
+  nome: "emerson pessoa",
+  notaFinal: "10",
+  celular: "(99)9999-9999",
+  dataNascimento: "09/01/1974",
 };
+*/
 
-const updateAluno = (index, aluno) => {
-  const dbAluno = readAluno();
-  dbAluno[index] = aluno;
-  setLocalStorage(dbAluno);
-};
-
-const readAluno = () => getLocalStorage();
-
+//-=-=-=-CREATE-=-=-=-
 const createAluno = (aluno) => {
-  const dbAluno = getLocalStorage();
-  dbAluno.push(aluno);
-  setLocalStorage(dbAluno);
+    // const db_aluno = []; //não deve ser um array vazio
+    const db_aluno = readAluno();
+    console.log(db_aluno);
+    db_aluno.push(aluno); //inseri o aluno ao banco de dados
+    localStorage.setItem("aluno", JSON.stringify(db_aluno)); //envia para o banco de dados
+};
+//para criar no console: createAluno(tempAluno)
+
+//-=-=-=-READ-=-=-=-
+const readAluno = () => JSON.parse(localStorage.getItem("aluno")) ?? []; //faz a leitura ao banco de dados
+//??[]  - interrogacao significa que for falso ou nulo ou vazio retorna um array vazio
+//para ler no console: readAluno(tempAluno)
+
+//-=-=-=-UPDATE-=-=-=-
+const updateAluno = (index, aluno) => {
+    const db_aluno = readAluno(); //faz a leitura do banco de dados, no caso o array.
+    db_aluno[index] = aluno; //db_aluno(array) na posicao index recebe um novo aluno
+    localStorage.setItem("aluno", JSON.stringify(db_aluno)); //envia o novo aluno ao banco de dados
+};
+//para atualizar no console: updateAluno(1,tempAluno)
+
+//-=-=-=-DELETE-=-=-=-
+const deleteAluno = (index) => {
+    const db_aluno = readAluno(); //faz a leitura do banco de dados
+    db_aluno.splice(index, 1); //pega o banco de dados a partir do seu index desejado e exclui somente 1
+    localStorage.setItem("aluno", JSON.stringify(db_aluno)); //atualiza o banco de dados
+};
+//para deletar no console: deleteAluno(2) = o "2" como exemplo do index;
+
+//-=-=-=-INTEGRAÇÃO COM O LAYOUT-=-=-=-=-
+
+const clearInputs = () => {
+    //chama todos os campos
+    const inputs = document.querySelectorAll(".modal-field"); //vai buscar todos os campos do input com array vazio
+    inputs.forEach((input) => {
+        input.value = "";
+    }); //para varrer todos os campos e retornar os campos vazios
 };
 
-const isValidFields = () => {
-  return document.getElementById("form").reportValidity();
+const isValid = () => {
+    return document.getElementById("form").reportValidity(); //essa função reportValidity() retorna verdadeiro para todos requisitos do HTML que foram atendidos
 };
-
-//Interação com o layout
-
-const clearFields = () => {
-  const fields = document.querySelectorAll(".modal-field");
-  fields.forEach((field) => (field.value = ""));
-  document.getElementById("nome").dataset.index = "new";
-};
-
 const saveAluno = () => {
-  debugger;
-  if (isValidFields()) {
-    const aluno = {
-      nome: document.getElementById("nome").value,
-      number: document.getElementById("number").value,
-      celular: document.getElementById("celular").value,
-      dataNascimento: document.getElementById("dataNascimento").value,
-    };
-    const index = document.getElementById("nome").dataset.index;
-    if (index == "new") {
-      createAluno(aluno);
-      updateTable();
-      closeModal();
-      alert("Cadastrado realizado com sucesso!");
-    } else {
-      updateAluno(index, aluno);
-      updateTable();
-      closeModal();
+    //verifica se os campos foram preenchidos
+    if (isValid()) {
+        //captura todos do dados dos campos e converte em json
+        const aluno = {
+            nome: document.getElementById("name").value,
+            notaFinal: document.getElementById("number").value,
+            celular: document.getElementById("celular").value,
+            dataNascimento: document.getElementById("dateBirth").value,
+        };
+        const index = document.getElementById("name").dataset.index; //invés de pegar o value, pega o dataset.index no input do HTML
+        //através do index, dá pra saber se pode salvar ou editar
+        if (index == "new") {
+            //e depois,cadatra o aluno
+            createAluno(aluno);
+            // clearInputs(); //limpa os campos
+            updateTable(); //para atualizar os dados no HTML
+            alert(
+                `Aluno(a) ${capitalizeFirst(
+                    aluno.nome,
+                )}, cadastrado com sucesso!`,
+            );
+            closeModal(); //fecha o modal
+            // console.log("Cadastrado com sucesso!"); //só entra se todos os requisitos do form forem atendidos
+        } else {
+            // console.log("Editando...")
+            updateAluno(index, aluno); //atualiza o aluno
+            updateTable(); //atualiza a tabela
+            closeModal(); //fecha a modal
+        }
     }
-  }
 };
 
 //configuração de data
@@ -77,123 +104,162 @@ const saveAluno = () => {
   day: "numeric",
 })
 */
-//Adicionar máscara no input
+//-=-=-=-Adicionar máscara no input-=-=-=-
 const celular = document.querySelector("#celular");
 
 celular.addEventListener("keyup", (event) => {
-  let start = celular.selectionStart; //initial cursor position
-  let end = celular.selectionEnd; //final cursor position
-  //if the start and end positions of the cursor are the same, it means that there is no selection range
+  let start = celular.selectionStart; 
+  let end = celular.selectionEnd; 
   if (start == end) {
-    //this conditional prevents the function call when the user makes a selection range in the input EX:. keys (Ctrl + A)
     formMask("__ _____-____", "_", event, start);
   }
 });
 
 function formMask(mask, char, event, cursor) {
-  const vetMask = mask.split(""); //transform mask into vector to use specific functions, like filter()
+  const vetMask = mask.split(""); 
   const onlyNumbers = celular.value
     .split("")
     .filter((value) => !isNaN(value) && value != " ");
   const key = event.keyCode || event.which;
-  const backspaceAndArrowKeys = [8, 37, 38, 39, 40]; //code backspace and arrow keys
+  const backspaceAndArrowKeys = [8, 37, 38, 39, 40]; 
   const clickedOnTheBackspaceOrArrowsKeys =
     backspaceAndArrowKeys.indexOf(key) >= 0;
-  const charNoMod = [".", "-"]; //characters that do not change
+  const charNoMod = [".", "-"]; 
   const cursorIsCloseToCharNoMod = charNoMod.indexOf(vetMask[cursor]) >= 0;
 
   onlyNumbers.forEach((value) =>
     vetMask.splice(vetMask.indexOf(char), 1, value)
-  ); //change '#' to numbers
+  ); 
 
   celular.value = vetMask.join("");
 
   if (!clickedOnTheBackspaceOrArrowsKeys && cursorIsCloseToCharNoMod) {
-    //increment the cursor if it is close to characters that do not change
     celular.setSelectionRange(cursor + 1, cursor + 1);
   } else {
     celular.setSelectionRange(cursor, cursor);
   }
 }
-// converter a 1a letra de cada palavra em maiúscula
-const capitalizeFirst = (str) => {
-  const subst = str.toLowerCase().replace(/(?:^|\s)\S/g, function (a) {
-    return a.toUpperCase();
-  });
-  return subst;
-};
 
-const createRow = ({ nome, number, celular, dataNascimento }, index) => {
-  const newRow = document.createElement("tr");
-  newRow.innerHTML = `
+//-=-=-converter a 1a letra de cada palavra em maiúscula-=-=-
+const capitalizeFirst = (string) => {
+    const substitute = string.toLowerCase().replace(/(?:^|\s)\S/g, (item) => {
+        return item.toUpperCase();
+    });
+    return substitute;
+};
+//-=-=-=-TRAZER OS DADOS PARA O HTML para preencher a tabela
+//toda vez que a tela carregar, já chama o método para atualizar a tabela
+const row = ({ nome, notaFinal, celular, dataNascimento }, index) => {
+    //os elementos e indice
+    //não existe na memória(no HTML), ou melhor, não existe no DOM
+    const newRow = document.createElement("tr"); //criando uma tr vazia
+    //campos do array aluno de saveAluno()
+    //preenche com newRow
+    newRow.innerHTML = `
         <td>${capitalizeFirst(nome)}</td>
-        <td>${Intl.NumberFormat("pt-br").format(number)}</td>
+        <td>${Intl.NumberFormat("pt-br").format(notaFinal)}</td>
         <td>${celular.replace(/^(\d{2})(\d{5})(\d{4})/, "($1) $2-$3")}</td>
         <td>${dataNascimento.split("-").reverse().join("/")}</td>
         <td>
             <button type="button" class="button green" id="edit-${index}">Editar</button>
-            <button type="button" class="button red" id="delete-${index}" >Excluir</button>
+            <button type="button" class="button red" id="delete-${index}">Excluir</button>
+            
         </td>
+        
     `;
-  document.querySelector("#tableAluno>tbody").appendChild(newRow);
+    //<button type="button" class="button green" data-action="edit">Editar</button>
+    //<button type="button" class="button red" data-action="delete">Excluir</button>
+    //obs: data-action: é dataset substituido por id
+
+    //atributo personalizado: data-action="edit"
+    //atributo personalizado: data-action="delete"
+
+    //capturando a tbody e inserir dentro da tbody da tabela um filho que são a novas linhas
+    document.querySelector("#tableAluno>tbody").appendChild(newRow); //tableAluno id do form
+    //executar no console: updateTable()
 };
 
+//-=-=-LIMPAR A TABELA PARA DUPLICAR APÓS ATUALIZAR A TABELA
 const clearTable = () => {
-  const rows = document.querySelectorAll("#tableAluno>tbody tr");
-  rows.forEach((row) => row.parentNode.removeChild(row));
+    const rows = document.querySelectorAll("#tableAluno>tbody tr"); //captura as linhas e converte em nodeList
+    //testando no console: document.querySelectorAll("#tableAluno>tbody tr");
+    rows.forEach((item) => {
+        //pega o pai da linha que é tbody(parentNode)
+        item.parentNode.removeChild(item); //removeChild() - remove o filho chamado de item
+    });
 };
-
+//-=-=-=-ATUALIZAR TABELA-=-=-=-
 const updateTable = () => {
-  const dbAluno = readAluno();
-  clearTable();
-  dbAluno.forEach(createRow);
+    //apaga tabela anterior e cria
+    //faz leitura e cria uma linha com cada um dos alunos
+    const db_aluno = readAluno(); //faz leitura dos dados do locastorage
+    clearTable(); //para não duplicar a tabela
+    db_aluno.forEach(row); // preenche a tabela onde cada um dos alunos deve ser uma linha
+};
+updateTable(); //para chamar a função após definir o método
+//para atualizar no console: updateAluno()
+
+//-=-=-=-EDITAR A TABELA-=-=-=-
+
+//para preecher os campos
+const completeInput = ({ nome, notaFinal, celular, dataNascimento, index }) => {
+    document.getElementById("name").value = nome;
+    document.getElementById("number").value = notaFinal;
+    document.getElementById("celular").value = celular;
+    document.getElementById("dateBirth").value = dataNascimento;
+    document.getElementById("name").dataset.index = index; //captura o dataset do name para mandar o index do aluno
 };
 
-const fillFields = (aluno) => {
-  document.getElementById("nome").value = aluno.nome;
-  document.getElementById("number").value = aluno.number;
-  document.getElementById("celular").value = aluno.celular;
-  document.getElementById("dataNascimento").value = aluno.dataNascimento;
-  document.getElementById("nome").dataset.index = aluno.index;
+const editIndex = (index) => {
+    const aluno = readAluno()[index]; //ler os alunos apontando pelo index do array(readAluno)
+    aluno.index = index; //atribuindo o index ao aluno
+    console.log(aluno);
+    completeInput(aluno); //preencher campos
+    openModal(); //após preencher os campos, abri o modal
 };
+//-=-=-=-EDITA E DELETA-=-=-=-
+const edit = (event) => {
+    //pega o evento do click
+    // console.log(event)
+    // console.log(event.target);
+    if (event.target.type == "button") {
+        //para pegar somente o click do evento do tipo button
+        // console.log(event.target.type); //captura o evento com alvo do tipo type
+        // console.log(event.target.dataset.action); //dataset - é o nome da propriedade que pega os dados e action nome da propriedade especificado no data
+        // console.log(event.target.id.split("-")); //split converte o evento em array
+        const [action, index] = event.target.id.split("-");
+        console.log(action, index);
+        if (action == "edit") {
+            editIndex(index);
+            // console.log("Editando o(a) aluno(a)");
+        } else {
+            const aluno = readAluno()[index]; //faz a leitura do array e captura o aluno pelo index
+            const response = confirm(
+                `Tem certeza que deseja excluir ${capitalizeFirst(
+                    aluno.nome,
+                )}?`,
+            );
 
-const editClient = (index) => {
-  const aluno = readAluno()[index];
-  aluno.index = index;
-  fillFields(aluno);
-  openModal();
-};
-
-const editDelete = (event) => {
-  if (event.target.type == "button") {
-    const [action, index] = event.target.id.split("-");
-
-    if (action == "edit") {
-      editClient(index);
-    } else {
-      const aluno = readAluno()[index];
-      const response = confirm(
-        `Deseja realmente excluir o aluno ${aluno.nome}`
-      );
-      if (response) {
-        deleteAluno(index);
-        updateTable();
-      }
+            //testando se for verdadeiro
+            if (response) {
+                // console.log("Deletando o(a) aluno(a)");
+                deleteAluno(index); //deleta mandando o index
+                updateTable(); //após excluir, atualiza a tabela
+                // alert("Aluno(a) excluído com sucesso!");
+            }
+        }
     }
-  }
+    //dataset - é o nome da propriedade onde pega os dados e action mostra o tipo de ação que eu quero
 };
 
-updateTable();
-
-// Eventos
+//-=-=-=-=- Eventos -=-=-=-=-
 document.getElementById("cadastrarAluno").addEventListener("click", openModal);
 
 document.getElementById("modalClose").addEventListener("click", closeModal);
 
 document.getElementById("salvar").addEventListener("click", saveAluno);
 
-document
-  .querySelector("#tableAluno>tbody")
-  .addEventListener("click", editDelete);
-
 document.getElementById("cancelar").addEventListener("click", closeModal);
+
+document.querySelector("#tableAluno>tbody").addEventListener("click", edit);
+//capturar botão editar através do pai que é o tbody dentro da tabela
